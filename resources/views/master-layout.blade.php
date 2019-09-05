2334d1e64d669d3d783db70bc0a66c7157809961
xuanhuy9930 <xuanhuy9930@gmail.com>
2019-09-05 10:17:06 +0700

    ...

--- resources/views/master-layout.blade.php
+++ resources/views/master-layout.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('library/OwlCarousel2/owl.theme.default.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     
 
     @yield('css')
@@ -29,8 +30,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/d2f7a09671.js"></script>
-
-
+    <script src="js/style.js"></script>
     @yield('js')
 </body>
 </html>
